0d24f3145e7129e5ea6a3a08df3ce7e048056aa1
Alper Çetin <78817506+alper402cetin@users.noreply.github.com>
2022-09-11 17:03:57 +0300

    Weather App With MVI Architecture

--- Full_Stacks/MVI_WeatherApp/app/src/main/java/com/plcoding/weatherapp/data/remote/WeatherDto.kt
+++ Full_Stacks/MVI_WeatherApp/app/src/main/java/com/plcoding/weatherapp/data/remote/WeatherDto.kt
@@ -2,7 +2,7 @@ package com.plcoding.weatherapp.data.remote
 
 import com.squareup.moshi.Json
 
-data class WeatherDto (
+data class WeatherDto(
     @field:Json(name = "hourly")
     val weatherData: WeatherDataDto
 )
